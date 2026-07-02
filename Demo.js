ea2f3569dbe6c3b7e79a12d17c92fe237e37a542
DiwaliCracker <147524132+DiwaliCracker@users.noreply.github.com>
2026-07-02 15:34:11 +0530

    Update Demo.js

--- Demo.js
+++ Demo.js
@@ -1,32 +1,39 @@
-{
-        img: "https://media-public-ht.project1content.com/m=eaSaaTbWx/f28/84c/4d1/5e3/488/99b/6e2/572/61d/963/c3/poster/poster_01.jpg",
-        video: "https://prog-public-ht.project1content.com/f28/84c/4d1/5e3/488/99b/6e2/572/61d/963/c3/mediabook/mediabook_720p.mp4",
+const sections = [
+  {
+    logo: "",
+    items: [
+                  {
+        img: "https://abc.com/v2/imagedb/shoots/107641/public/galleryimage/107641_ImageDB01_thumb.png?w=1920",
+        video: "https://abc2.com/v2/imagedb/shoots/107641/public/trailer/107641_trailer_high.mp4",
         iframe: "",
-        name: "Delilah Want's Stepdad's Cock",
-        altname: "Delilah Want's Stepdad's Cock Delilah Dagger, Peter Fitzwell BangBros 18 BangBros bangbros.com",
-        link: "https://playerjs3.onrender.com/player.html?file=[360p]https://soski.tv/mp4/files/77757974/360,[480p]https://soski.tv/mp4/files/77757974/480,[720p]https://soski.tv/mp4/files/77757974/720,[1080p]https://soski.tv/mp4/files/77757974/1080&poster=https://media-public-ht.project1content.com/m=eaSaaTbWx/f28/84c/4d1/5e3/488/99b/6e2/572/61d/963/c3/poster/poster_01.jpg&thumbnails=",
-        date: "09/02/2025",
-        time: "39:23",
-        badge: "FHD+SOSKI",
+        name: "I Am Your Goddess",
+        altname: "I Am Your Goddess Robert , john",
+        link: "https://player.com/player.html?file=&poster=https://abc.com/v2/imagedb/shoots/107641/public/galleryimage/107641_ImageDB01_thumb.png?w=1920&thumbnails=",
+        date: "03/02/2026",
+        time: "20:51",
+        badge: "HD+EMBED",
         actors: [
-          { name: "Delilah Dagger", link: "" },
-          { name: "Peter Fitzwell", link: "" },
+          { name: "Robert", link: "" },
+          { name: "john", link: "" },
           { name: "Screenshots", link: "" }
         ]
       },
       {
-        img: "https://media-public-ht.project1content.com/m=eaSaaTbWx/cfd/769/56f/43a/412/aa8/409/fce/969/a73/cc/poster/poster_01.jpg",
-        video: "https://prog-public-ht.project1content.com/cfd/769/56f/43a/412/aa8/409/fce/969/a73/cc/mediabook/mediabook_720p.mp4",
+        img: "https://abc.com/imagedb/107431/galleryImage/107431_imagedb_01_thumb.png?w=1920",
+        video: "",
         iframe: "",
-        name: "Spreading Zen Over Essie",
-        altname: "Spreading Zen Over Essie Essie Gotback, Johnny Love MomIsHorny BangBros bangbros.com",
-        link: "https://playerjs3.onrender.com/player.html?file=[360p]https://soski.tv/mp4/files/74321356/360,[480p]https://soski.tv/mp4/files/74321356/480,[720p]https://soski.tv/mp4/files/74321356/720,[1080p]https://soski.tv/mp4/files/74321356/1080&poster=https://media-public-ht.project1content.com/m=eaSaaTbWx/cfd/769/56f/43a/412/aa8/409/fce/969/a73/cc/poster/poster_01.jpg&thumbnails=",
-        date: "06/09/2024",
-        time: "45:19",
-        badge: "FHD+SOSKI",
+        name: "Bound and Tamed",
+        altname: "avengers scarlett, samantha",
+        link: "https://player.com/player.html?file=&poster=https://abc.com/imagedb/107431/galleryImage/107431_imagedb_01_thumb.png?w=1920&thumbnails=",
+        date: "02/02/2026",
+        time: "",
+        badge: "",
         actors: [
-          { name: "Essie Gotback", link: "" },
-          { name: "Johnny Love", link: "" },
+          { name: "scarlett", link: "" },
+          { name: "samantha", link: "" },
           { name: "Screenshots", link: "" }
         ]
-}
+      }
+  ]
+    }
+  ];
